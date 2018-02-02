ba237024c93753ac1fafcda229a0ad085ddd5df3
charleschang <im741314@gmail.com>
2018-02-02 16:45:47 +0800

    refactor strings

---
+++ src/hohuan/addressbook/Configuration.java
@@ -0,0 +1,10 @@
+public class Config {
+  /* We use a String array to store details of a single person.
+  * The constants given below are the indexes for the different data elements of a person
+  * used by the internal String[] storage format.
+  * For example, a person's name is stored as the 0th element in the array.
+  */
+  public static final int PERSON_DATA_INDEX_NAME = 0;
+  public static final int PERSON_DATA_INDEX_PHONE = 1;
+  public static final int PERSON_DATA_INDEX_EMAIL = 2;
+}
